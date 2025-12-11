e341fc3c3c1f628805d7b4a1de68e10d9fd5e4a6
andrea velazquez <andreavelazquez101@gmail.com>
2025-12-10 21:41:47 -0700

    final

---
+++ src/javascripts/main.js
@@ -0,0 +1,6 @@
+require.context('../fonts/', true, /\.(eot|ttf|woff|woff2)$/i);
+require.context('../images/', true, /\.(png|jpg|jpeg|gif|svg|mp4)$/i);
+require('../stylesheets/main.scss');
+
+// Optional: just to confirm JS is running
+console.log('Wonder Studios JS loaded');
